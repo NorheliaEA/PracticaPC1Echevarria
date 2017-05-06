1f0c0d489dee35da215d74c7a868f3b05f3746dd
NorheliaEA <u201220133@upc.edu.pe>
2017-05-06 10:21:02 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,36 @@
 <!DOCTYPE html>
- <html>
- <head>
-  <title>PHP Starter Application</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <link rel="stylesheet" href="style.css" />
- </head>
- <body>
-	 <div class="container">
-		<img src="images/banner1.png" />
-		
-		</div>
-  
-  <table>
-   <?php
-  $servername = "us-cdbr-iron-east-03.cleardb.net";
-  $username = "b74ba3320e82ec";
-  $password = "2d194843";
-  $dbname = "ad_bb6ddb2f34daf52";
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  // Check connection
-  if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-  }
-  $sql = "SELECT * from servicios";
-  $result = $conn->query($sql);
-  if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {?>
-      <tr>
-      <td><?php echo $row['codigo_servicio']?></td>
-      <td><?php echo $row['nombre_servicio']?></td>
-      <td><?php echo $row['descripcion_servicio']?></td>
-	<td><?php echo $row['imagen_servicio']?></td>
-      </tr>
-     <?php }
-  } else {
-      echo "0 results";
-  }
-  $conn->close();
- ?> 
- </table>
-		 
-		 <div class="container">
-		<img src="images/banner2.png" />
-		
-		</div>
- </body>
- </html>
+<html>
+<head>
+	<title>PHP Starter Application</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<?php
+	$servername = "us-cdbr-iron-east-03.cleardb.net";
+	$username = "b74ba3320e82ec";
+	$password = "2d194843";
+	$dbname = "ad_bb6ddb2f34daf52";
+	
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) {
+	  die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$sql = "SELECT * from servicios";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) { ?>
+	<table>
+	  <?php while($row = $result->fetch_assoc()) { ?>
+	  	<tr style="width: 50%; float: left; height: 114px;">
+	      <td><img src="<?php echo $row['imagen_servicio']; ?>" /></td>
+	      <td><b><?php echo $row['nombre_servicio']?></b> <br /> <?php echo $row['descripcion_servicio']?></td>
+	    </tr>
+	  <?php } ?>
+	</table>
+	<?php }  
+	$conn->close();
+	?> 
+	<img src="images/banner2.JPG"/>
+</body>
+</html>
