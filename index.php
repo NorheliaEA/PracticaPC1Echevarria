ad1fdc54418d6b910eea9794a6319c5eafab21be
NorheliaEA <u201220133@upc.edu.pe>
2017-05-06 10:30:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,31 +6,34 @@
 	<link rel="stylesheet" href="style.css" />
 </head>
 <body>
-	<?php
-	$servername = "us-cdbr-iron-east-03.cleardb.net";
-	$username = "b74ba3320e82ec";
-	$password = "2d194843";
-	$dbname = "ad_bb6ddb2f34daf52";
-	
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	if ($conn->connect_error) {
-	  die("Connection failed: " . $conn->connect_error);
-	}
-	
-	$sql = "SELECT * from servicios";
-	$result = $conn->query($sql);
-	if ($result->num_rows > 0) { ?>
-	<table>
-	  <?php while($row = $result->fetch_assoc()) { ?>
-	  	<tr style="width: 50%; float: left; height: 114px;">
-	      <td><img src="<?php echo $row['imagen_servicio']; ?>" /></td>
-	      <td><b><?php echo $row['nombre_servicio']?></b> <br /> <?php echo $row['descripcion_servicio']?></td>
-	    </tr>
-	  <?php } ?>
-	</table>
-	<?php }  
-	$conn->close();
-	?> 
-	<img src="images/banner2.JPG"/>
+	<div class="marco">
+		<img src="images/banner1.PNG" style="width: 100%; height: auto;" />
+		<?php
+		$servername = "us-cdbr-iron-east-03.cleardb.net";
+		$username = "b74ba3320e82ec";
+		$password = "2d194843";
+		$dbname = "ad_bb6ddb2f34daf52";
+		
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		if ($conn->connect_error) {
+		  die("Connection failed: " . $conn->connect_error);
+		}
+		
+		$sql = "SELECT * from servicios";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) { ?>
+		<table>
+		  <?php while($row = $result->fetch_assoc()) { ?>
+		  	<tr style="width: 50%; float: left; height: 114px;">
+		      <td><img src="<?php echo $row['imagen_servicio'] ?>" /></td>
+		      <td><b><?php echo $row['nombre_servicio']?></b> <br /> <?php echo $row['descripcion_servicio']?></td>
+		    </tr>
+		  <?php } ?>
+		</table>
+		<?php }  
+		$conn->close();
+		?> 
+		<img src="images/banner2.PNG" style="width: 100%; height: auto;" />
+	</div>
 </body>
 </html>
